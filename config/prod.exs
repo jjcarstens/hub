61d7dcf22bc610bcbcde7ebea34671df0bd44bb4
Jon Carstens <jjcarstens@me.com>
2019-02-27 15:03:55 -0700

    force ssl in prod

--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,7 @@ use Mix.Config
 
 config :hub, HubWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   http: [:inet6, port: System.get_env("PORT") || 4000],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   server: true,
