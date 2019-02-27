5333bfea2c3a2a794f634c71905a312e9045aaaa
Jon Carstens <jjcarstens@me.com>
2019-02-27 14:41:41 -0700

    touchup prod.exs for deploy

--- config/prod.exs
+++ config/prod.exs
@@ -1,39 +1,21 @@
 use Mix.Config
 
-secrets = File.read!("config/prod.secrets")
-          |> String.split()
-          |> Enum.reduce(%{}, fn(str, acc) ->
-            [key,val] = String.split(":", trim: true)
-            Map.put(acc, String.to_atom(key), val)
-          end)
-
 config :hub, HubWeb.Endpoint,
-  http: [:inet6, port: secrets[:port] || 4000],
-  url: [host: secrets.host],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: secrets.secret_key_base,
-  server: true
+  http: [:inet6, port: System.get_env("PORT") || 4000],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  server: true,
+  url: [host: System.get_env("HOST")]
 
-config :hub, Hub.Repo.
+config :hub, Hub.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "hub_prod",
-  ssl: true,
-  pool_size: 2
+  pool_size: 2,
+  ssl: true
+
+config :ueberauth, Ueberauth.Strategy.Facebook.OAuth,
+  client_id: System.get_env("CLIENT_ID"),
+  client_secret: System.get_env("CLIENT_SECRET")
 
 # Do not print debug messages in production
 config :logger, level: :info
-
-# config :gigalixir_getting_started, GigalixirGettingStartedWeb.Endpoint,
-#   load_from_system_env: true,
-#   http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
-#   server: true, # Without this line, your app will not start the web server!
-#   secret_key_base: "${SECRET_KEY_BASE}",
-#   url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
-#   cache_static_manifest: "priv/static/cache_manifest.json"
-
-# config :gigalixir_getting_started, GigalixirGettingStarted.Repo,
-#   adapter: Ecto.Adapters.Postgres,
-#   url: "${DATABASE_URL}",
-#   database: "",
-#   ssl: true,
-#   pool_size: 2 # Free tier db on
